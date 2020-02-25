a020ebc38233e957a6821d47f47da3cac367c882
Christopher Campbell <chris@launchbadge.com>
2020-02-25 02:07:26 -0800

    Back to TSX; SFC will not work with just Babel

---
+++ src/ui/config/router.ts
@@ -0,0 +1,39 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import Home from "../views/Home";
+
+interface Position {
+    x: number;
+    y: number;
+}
+
+type PositionResult = Position | { selector: string; offset?: Position } | void;
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home
+    }
+]
+
+const router = new VueRouter({
+    mode: "hash",
+    base: "/",
+    routes,
+    scrollBehavior(_, __, savedPosition): PositionResult {
+        if (savedPosition != null) {
+            return savedPosition;
+        }
+
+        return {
+            x: 0,
+            y: 0
+        }
+    }
+});
+
+export default router;
