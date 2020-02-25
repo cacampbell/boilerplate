95b015c1ebb132d8c3c78d5012866d05c1f3416f
Christopher Campbell <chris@launchbadge.com>
2020-02-24 21:59:15 -0800

    Fixed Polyfill issue by simply removing polyfills (via removing @vue/preset-app)

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,13 +2,13 @@ import { defineComponent } from "@vue/composition-api";
 
 export default defineComponent({
     setup() {
-        return (_, context): JSX.Element => (
+        return (): JSX.Element => (
             <div class="app">
                 <header><h1 class="layout-note">Header</h1></header>
                 <nav><h2 class="layout-note">Navigation</h2></nav>
                 <main>
                     <h2 class="layout-note">Main Content</h2>
-                    <router-view key={ context.root.$route.path } />
+                    <router-view />
                 </main>
                 <footer><h2 class="layout-note">Footer</h2></footer>
             </div>
