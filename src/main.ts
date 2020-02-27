0418b52c8ecb618eaa952c5f9a6f10becae5079a
Christopher Campbell <chris@launchbadge.com>
2020-02-26 23:57:19 -0800

    upate

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import Vue, { VNode } from "vue";
 import "./config/composition";
 import i18n from "./config/i18n";
 import router from "./config/router";
-import App from "./ui/App.vue";
+import App from "./ui/App";
 
 new Vue({
     router,
